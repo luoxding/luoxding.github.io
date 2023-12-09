396c12acb16bb3ef30b6ee420f871028275f9816
luoxding <anthony_lambert@live.com>
2023-12-09 15:28:15 +0800

    repo backup: 2023-12-09 15:28:15

--- content/post/hello-hugo/index.md
+++
@@ -1,9 +0,0 @@
----
-title: "Hello Hugo"
-date: 2022-03-19T00:17:57+08:00
-draft: true
----
-
-## hello hugo
-
-> enjoy!
--- git.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-set log=%DATE%%TIME%
-echo %log%
-hugo -D
-cd public
-git add .
-git commit -m "%log% update"
-git push -u origin main
-pause 
