bef667acffd620fea5443c5c1fc57f7ffc72cc9d
luoxding <anthony_lambert@live.com>
2024-05-06 20:11:50 +0800

    Site updated: 2024-05-06 20:11:50

--- 03-deploy2server.sh
+++ 03-deploy2server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 USER=root
 HOST=23.94.38.114
-DIR=/var/www/hugo/   # the directory where your web site files should go
+DIR=/var/opt/blog/   # local dir the directory where your web site files should go
 
 hugo && rsync -avz --delete public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
 # sudo hugo && rsync -avz --delete ./public/ ${DIR} # this will delete everything on the server that's not in the local public folder 
